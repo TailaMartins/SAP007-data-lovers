de92c0258b42c0139012ed43adc91f81788712d5
Taila Martins <tailamartins74@gmail.com>
2022-03-10 12:25:37 -0300

    atualizaçao de funcoes de calculo e voltar ao topo

--- src/data.js
+++ src/data.js
@@ -1,24 +1,38 @@
-export const filterData = (data, condition) => {
-  return data.filter((personagens) => (personagens[condition[0]] == condition[1]));
-};
+export const filterGenero = (data, genero) => data.filter (personagens => {
+  return personagens.gender === genero;
+})
+
+export const filterStatus = (data, condition) => data.filter (personagens =>{
+  return personagens.status === condition;
+})
+
+export const filterEspecie = (data, especie) => data.filter (personagens =>{
+  return personagens.species === especie;
+})
 
 export const filternome =  (data, buscaNome) => data.filter (personagens => {
   return personagens.name.toLowerCase().includes(buscaNome.toLowerCase());
-})
+});
 
-export const sortBy = (data, order) => {
+export const sortByName = (data, order) => {
   if (order === "ordenarAZ") {
-    return data.sort((a, b) => a.name < b.name ? -1 : 1)
-  } else {
+  return data.sort((a, b) => a.name < b.name ? -1 : 1)
+} else {
     return data.sort((a, b) => a.name > b.name ? -1 : 1)
-  } 
+} 
 };
+
 
 export const computeStats = (data) => {
-  return Math.round(100 * ( data.selectedLenght / data.totalLenght))
-};
+  return data.length;
+}
+
+
+
+
+
  
-//   
+ 
 
 
 
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,33 +1,59 @@
-import { filterData, sortData, computeStats } from '../src/data.js';
+import { filterData } from '../src/data.js';
+//, sortByName, computeStats//
+ const personagens = [
+ { "id": 1,
+  "name": "Rick Sanchez",
+  "status": "Alive",
+  "species": "Human",
+  "gender": "Male",
+},
+{
+  "id": 2,
+  "name": "Morty Smith",
+  "status": "Alive",
+  "species": "Human",
+  "type": "",
+  "gender": "Male"
+},
+{ "id": 3,
+  "name": "Summer Smith",
+  "status": "Alive",
+  "species": "Human",
+  "type": "",
+  "gender": "Female",
+},
+
+]
 
 
-describe('filterData', () => {
-  it('is a function', () => {
+
+describe('filtrar objetos dos selects',() => {
+  it('should be a function', () => {
     expect(typeof filterData).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('retornar filtrado a opção selelecionada', () => {
+    expect(filterData(personagens.gender == personagens.Female)).toBe('Summer Smith');
   });
 });
 
 
-describe('sortData', () => {
-  it('is a function', () => {
-    expect(typeof sortData).toBe('function');
-  });
+// describe('sortByName', () => {
+//   it('is a function', () => {
+//     expect(typeof sortByName).toBe('function');
+//   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
+//   it('returns `anotherExample`', () => {
+//     expect(anotherExample()).toBe('OMG');
+//   });
+// });
 
-describe('computeStats', () => {
-  it('is a function', () => {
-    expect(typeof computeStats).toBe('function');
-  });
+// describe('computeStats', () => {
+//   it('is a function', () => {
+//     expect(typeof computeStats).toBe('function');
+//   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
+//   it('returns `anotherExample`', () => {
+//     expect(anotherExample()).toBe('OMG');
+//   });
+// });
